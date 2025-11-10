0a121e905d59bdbe4b224097dfe9d2321e92fbc6
Erik Schulz <git@eschulz.eu>
2025-11-10 13:04:06 +0100

    Day 3 done

---
+++ 2025/quest_03.py
@@ -0,0 +1,34 @@
+"""Third Day"""
+
+from collections import Counter
+
+
+def stack_crates(l: list[int]) -> int:
+    unique_crates = set(l)
+    return sum(unique_crates)
+
+
+def pack_mushroom(l: list[int]) -> int:
+    unique_crates = list(set(l))
+    return sum(unique_crates[:20])
+
+
+def minimize_sets(l: list[int]) -> int:
+    _, sets = Counter(l).most_common(1)[0]
+    return sets
+
+
+if __name__ == "__main__":
+    from utils import load_input
+
+    INPUT1 = load_input(2025, 3, 1)
+    INPUT1 = [int(n) for n in INPUT1.split(",")]
+    print(stack_crates(INPUT1))
+
+    INPUT2 = load_input(2025, 3, 2)
+    INPUT2 = [int(n) for n in INPUT2.split(",")]
+    print(pack_mushroom(INPUT2))
+
+    INPUT3 = load_input(2025, 3, 3)
+    INPUT3 = [int(n) for n in INPUT3.split(",")]
+    print(minimize_sets(INPUT3))
