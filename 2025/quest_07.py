b16c214370494708674508ce5e7c740d5dc0480d
Erik Schulz <git@eschulz.eu>
2025-11-12 14:38:57 +0100

    Day 7 done

---
+++ 2025/quest_07.py
@@ -0,0 +1,81 @@
+"""Seventh Day"""
+
+from utils import load_input
+
+
+# Helper Functions
+def get_names(notes: str) -> list[str]:
+    return notes.splitlines()[0].split(",")
+
+
+def get_rules(notes: str) -> dict[str, list[str]]:
+    ruledict = {}
+    rules = notes.splitlines()[2:]
+    for rule in rules:
+        followers = rule[4:].split(",")
+        ruledict[rule[0]] = followers
+    return ruledict
+
+
+def check_name(name: str, rules: dict[str, list[str]]) -> bool:
+    for i in range(len(name) - 1):
+        if name[i + 1] not in rules[name[i]]:
+            return False
+    return True
+
+
+# 1
+def find_name(notes: str) -> str:
+    names = get_names(notes)
+    rules = get_rules(notes)
+    for name in names:
+        if check_name(name, rules):
+            return name
+    return "Error"
+
+
+# 2
+def find_name_sum(notes: str) -> int:
+    total = 0
+    names = get_names(notes)
+    rules = get_rules(notes)
+    for i, name in enumerate(names):
+        if check_name(name, rules):
+            total += i + 1
+    return total
+
+
+# 3
+def build_names(notes: str) -> int:
+    names = []
+    prefixes = [name for name in get_names(notes) if check_name(name, get_rules(notes))]
+    rules = get_rules(notes)
+
+    def add_letters(name: str) -> None:
+        if 7 <= len(name) <= 11:
+            names.append(name)
+        if len(name) == 11:
+            return None
+
+        try:
+            for letter in rules[name[-1]]:
+                add_letters(name + letter)
+        except KeyError:
+            pass
+        return None
+
+    for prefix in prefixes:
+        add_letters(prefix)
+
+    return len(set(names))
+
+
+if __name__ == "__main__":
+    INPUT1 = load_input(2025, 7, 1)
+    print(find_name(INPUT1))
+
+    INPUT2 = load_input(2025, 7, 2)
+    print(find_name_sum(INPUT2))
+
+    INPUT3 = load_input(2025, 7, 3)
+    print(build_names(INPUT3))
