a1618be5db25dc7a96fdeede2dd3da8a0a9f7d7e
Yldrax <eschulz2100@gmail.com>
2025-11-18 16:30:08 +0100

    formatting

--- 2025/quest_11.py
+++ 2025/quest_11.py
@@ -2,6 +2,7 @@
 
 from utils import load_input
 
+
 def balance_formation(notes: list[int], maxrounds: int):
     formation = notes.copy()
     phase = 1
@@ -13,29 +14,28 @@ def balance_formation(notes: list[int], maxrounds: int):
         rounds += 1
         changed = False
         if phase == 1:
-            for i in range(len(formation)-1):
-                if formation[i] > formation[i+1]:
+            for i in range(len(formation) - 1):
+                if formation[i] > formation[i + 1]:
                     formation[i] -= 1
-                    formation[i+1] += 1
+                    formation[i + 1] += 1
                     changed = True
             if not changed:
                 phase = 2
 
         if phase == 2:
-            for i in range(len(formation)-1):
-                if formation[i] < formation[i+1]:
+            for i in range(len(formation) - 1):
+                if formation[i] < formation[i + 1]:
                     formation[i] += 1
-                    formation[i+1] -= 1
+                    formation[i + 1] -= 1
                     changed = True
             if not changed:
                 balanced = True
 
     checksum = 0
     for i, line in enumerate(formation):
-        checksum += (i+1)*line
-    
+        checksum += (i + 1) * line
 
-    return (checksum,rounds-1)
+    return (checksum, rounds - 1)
 
 
 if __name__ == "__main__":
@@ -46,4 +46,4 @@ if __name__ == "__main__":
     print(balance_formation(INPUT2, -1)[1])
 
     INPUT3 = [int(n) for n in load_input(2025, 11, 3).splitlines()]
-    #print(balance_formation(INPUT3, -1)[1])    #Way too inefficient
+    # print(balance_formation(INPUT3, -1)[1])    #Way too inefficient
